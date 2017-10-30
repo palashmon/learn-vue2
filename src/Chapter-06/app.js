1146fa0ac8b1e68ba5b1c11df8e04796ce88c094
Palash Mondal <palashm@live.in>
2017-10-30 20:16:20 +0530

    Keyboard events and modifiers Link: https://v1.vuejs.org/guide/events.html#Key-Modifiers

---
+++ src/Chapter-06/app.js
@@ -0,0 +1,15 @@
+// Creating a Vue Instance with new Vue()
+new Vue({
+    el: '#vue-app',
+    data: {
+
+    },
+    methods: {
+        logName() {
+            console.log('you entered your name');
+        },
+        logAge() {
+            console.log('you entered your age');
+        }
+    }
+});
