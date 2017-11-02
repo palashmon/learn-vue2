da9f5cd698d9f53f12f5df003ef44f0e1c621658
Palash Mondal <palashm@live.in>
2017-11-02 19:50:33 +0530

    Looping with v-for Link: https://vuejs.org/v2/guide/list.html

---
+++ src/Chapter-11/app.js
@@ -0,0 +1,16 @@
+// Creating a Vue Instance with new Vue()
+new Vue({
+    el: '#vue-app',
+    data: {
+        todos: [
+            { text: 'Learn JavaScript' },
+            { text: 'Learn Vue' },
+            { text: 'Build something awesome' }
+        ],
+        object: {
+            firstName: 'John',
+            lastName: 'Doe',
+            age: 30
+        }
+    }
+});
