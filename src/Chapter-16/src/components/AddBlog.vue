cf8905e85ba94e61028554bdb020128a90816934
Palash Mondal <palashm@live.in>
2017-11-30 11:40:44 +0530

    Select box binding Link: https://vuejs.org/v2/guide/forms.html#Select

--- src/Chapter-16/src/components/AddBlog.vue
+++ src/Chapter-16/src/components/AddBlog.vue
@@ -17,6 +17,10 @@
                 <label>Cheese</label>
                 <input type="checkbox" value="cheese" v-model="blog.categories" />
             </div>
+            <label>Author:</label>
+            <select v-model="blog.author">
+                <option v-for="author in authors" :key="author">{{ author }}</option>
+            </select>
         </form>
         <div id="preview">
             <h3>Preview blog</h3>
@@ -27,6 +31,7 @@
             <ul>
                 <li v-for="category in blog.categories" :key="category">{{ category }}</li>
             </ul>
+            <p>Author: {{ blog.author }}</p>
         </div>
     </div>
 </template>
@@ -39,8 +44,10 @@ export default {
             blog: {
                 title: '',
                 content: '',
-                categories: []
-            }
+                categories: [],
+                author: ''
+            },
+            authors: ['The Net Ninja', 'The Angular Avenger', 'The Vue Vindicator']
         };
     },
     methods: {}
