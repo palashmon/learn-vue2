70fc60ddc3e5fc86517c635b3859ee3f8a195673
Palash Mondal <palashm@live.in>
2017-12-05 20:33:51 +0530

    Added router-link component for navigation Specify the link by passing the 'to' prop. '<router-link>' will be rendered as an '<a>' tag by default

--- src/Chapter-16/src/App.vue
+++ src/Chapter-16/src/App.vue
@@ -1,5 +1,14 @@
 <template>
     <div>
+        <p>
+            <!-- use router-link component for navigation. -->
+            <!-- specify the link by passing the `to` prop. -->
+            <!-- `<router-link>` will be rendered as an `<a>` tag by default -->
+            <router-link to="/">Show Blogs</router-link>
+            <router-link to="/add">Add Blog</router-link>
+        </p>
+        <!-- route outlet -->
+        <!-- component matched by the route will render here -->
         <router-view></router-view>
     </div>
 </template>
